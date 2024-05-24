2adf26f8c8ba57f64e81751e17b104d069aea595
Eirene <eirene9612056@gmail.com>
2024-05-24 23:14:31 +0700

    .

--- Hunting Snake/main.cpp
+++ Hunting Snake/main.cpp
@@ -4,11 +4,16 @@
 int main() {
     system("color F0");
 
+    std::thread music(PlayMusic);
+
     ofstream load("LOADGAME.txt");
     load.close();
     ofstream rank("RANK.txt");
     rank.close();
 
     StartGame();
+
+    StopMusic();
+    music.join();
     return 0;
 }
